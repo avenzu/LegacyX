407f5a998c71ceeebcac851c71e1ca806c6b0ddc
Akash Patel <venzuakash4225@gmail.com>
2022-09-13 17:45:52 -0600

    added methods to perform basic math operations

---
+++ VirtualAddingMachine/VirtualAddingMachine/Client/Methods/calculations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace VirtualAddingMachine.Client.Methods
+{
+    public class calculations
+    {
+        public double Addition(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double Subtraction(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public double Multiplication(double num1, double num2)
+        {
+            return (num1 * num2);
+        }
+
+        public double Division(double num1, double num2)
+        {
+            return (num1 / num2);
+        }
+
+        public double Modulus(double num1, double num2)
+        {
+            return (num1 % num2);
+        }
+
+    }
+}
