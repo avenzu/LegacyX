cdb73b189b9c68ffb43a8775d949fc55003e2b29
Akash Patel <venzuakash4225@gmail.com>
2022-09-16 08:54:54 -0600

    added a method to set focus on textarea

---
+++ VirtualAddingMachine/VirtualAddingMachine/Client/wwwroot/jsUtility.js
@@ -0,0 +1,17 @@
+﻿// source: https://www.syncfusion.com/faq/blazor/general/how-do-i-generate-and-save-a-file-client-side-using-blazor
+function saveFile(name, Content) {
+    var link = document.createElement('a');
+    link.download = name;
+    link.href = "data:text/plain;charset=utf-8," + encodeURIComponent(Content)
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
+function invokePrint() {
+    window.print();
+}
+
+function focusTextArea(id) {
+    document.getElementById(id).focus();
+}
