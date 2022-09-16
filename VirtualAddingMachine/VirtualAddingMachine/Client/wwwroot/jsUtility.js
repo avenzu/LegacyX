cdc7c6174073fa2a4e4c2839cdec22f9589aa4d3
Akash Patel <venzuakash4225@gmail.com>
2022-09-16 10:52:01 -0600

    done final commenting

--- VirtualAddingMachine/VirtualAddingMachine/Client/wwwroot/jsUtility.js
+++ VirtualAddingMachine/VirtualAddingMachine/Client/wwwroot/jsUtility.js
@@ -1,4 +1,12 @@
-﻿// source: https://www.syncfusion.com/faq/blazor/general/how-do-i-generate-and-save-a-file-client-side-using-blazor
+﻿/**
+ * File: jsUtility.js
+ * Author: Akash Patel
+ * Date: 16 Sep 2022
+ * Summary : Utility file containing helper JS functions to perform certain tasks
+ */
+
+// source: https://www.syncfusion.com/faq/blazor/general/how-do-i-generate-and-save-a-file-client-side-using-blazor
+// helping function to download calculator history as a text file
 function saveFile(name, Content) {
     var link = document.createElement('a');
     link.download = name;
@@ -8,10 +16,12 @@ function saveFile(name, Content) {
     document.body.removeChild(link);
 }
 
+// allow to print calculator history
 function invokePrint() {
     window.print();
 }
 
+// set focus to the textarea which allows user to edit the calculation data
 function focusTextArea(id) {
     document.getElementById(id).focus();
 }
